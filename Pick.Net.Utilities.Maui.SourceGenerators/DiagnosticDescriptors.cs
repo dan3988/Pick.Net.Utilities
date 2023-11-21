5eb249c197a778229d66975c71a12ffca79acca1
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-11-21 21:12:53 +0000

    Changed error messages

--- Pick.Net.Utilities.Maui.SourceGenerators/DiagnosticDescriptors.cs
+++ Pick.Net.Utilities.Maui.SourceGenerators/DiagnosticDescriptors.cs
@@ -1,64 +1,94 @@
 ﻿using System.Collections.Immutable;
+using System.Text;
+
+using Pick.Net.Utilities.Maui.Helpers;
 
 namespace Pick.Net.Utilities.Maui.SourceGenerators;
 
 internal static class DiagnosticDescriptors
 {
-	private const string Category = "BindablePropertyGenerator";
+	private const string Prefix = "PNUM";
+	private const string Category = "Pick.Net.Utilities.Maui.SourceGenerators.BindablePropertyGenerator";
+
+	private static string EnumText<T>() where T : unmanaged, Enum
+	{
+		var names = Enum.GetNames(typeof(T));
+		return ListText(names);
+	}
+
+	private static string ListText(string[] values)
+	{
+		if (values.Length < 2)
+			throw new ArgumentException("Must supply at leas 2 values", nameof(values));
+		
+		var sb = new StringBuilder();
+		var i = 1;
+
+		sb.Append(values[0]);
+
+		while (true)
+		{
+			var value = values[i];
+			if (++i == values.Length)
+				return sb.Append(" or ").Append(value).ToString();
+
+			sb.Append(", ").Append(value);
+		}
+	}
 
 	public static readonly DiagnosticDescriptor BindablePropertyEmptyPropertyName = new(
-		"DNU0001",
-		"PropertyName cannot be null or empty",
-		"PropertyName cannot be null or empty",
+		Prefix + "0001",
+		"Bindable property name cannot be null or empty",
+		"Bindable property name cannot be null or empty",
 		Category,
 		DiagnosticSeverity.Error,
 		true,
-		"PropertyName cannot be null or empty.");
+		"Cannot create a BindableProperty with a null or empty property name.");
 
 	public static readonly DiagnosticDescriptor BindablePropertyInvalidPropertyName = new(
-		"DNU0002",
-		"PropertyName is not a valid .NET identifier",
-		"PropertyName is not a valid .NET identifier: {0}",
+		Prefix + "0002",
+		"Supplied property name is not a valid .NET identifier",
+		"Property name is not a valid .NET identifier: {0}",
 		Category,
 		DiagnosticSeverity.Error,
 		true,
-		"PropertyName is not a valid .NET identifier.");
+		"Cannot create a BindableProperty with a property name that is not a valid .NET identifier.");
 
 	public static readonly DiagnosticDescriptor BindablePropertyInvalidDefaultMode = new(
-		"DNU0003",
-		"Invalid BindingMode value for [BindableProperty]",
-		"Invalid BindingMode value: {0}",
+		Prefix + "0003",
+		"Supplied DefaultMode value is not a known value",
+		"Unknown DefaultMode value: {0}",
 		Category,
 		DiagnosticSeverity.Error,
 		true,
-		"Invalid BindingMode value.");
+		$"Must use a valid BindingMode value ({EnumText<BindingMode>()}).");
 
 	public static readonly DiagnosticDescriptor BindablePropertyInvalidVisibility = new(
-		"DNU0004",
-		"Invalid PropertyVisibility value for [BindableProperty]",
-		"Invalid PropertyVisibility value: {0}",
+		Prefix + "0004",
+		"Supplied PropertyVisibility value is not a known value",
+		"Unknown PropertyVisibility value: {0}",
 		Category,
 		DiagnosticSeverity.Error,
 		true,
-		"Invalid PropertyVisibility value.");
+		$"Must use a valid PropertyVisibility value ({EnumText<PropertyVisibility>()}).");
 
 	public static readonly DiagnosticDescriptor BindablePropertyDefaultValueNotSupported = new(
-		"DNU0005",
-		"DefaultValue not supported for given property type on [BindableProperty]",
-		"DefaultValue not supported for property type {0}",
+		Prefix + "0005",
+		"DefaultValue not supported for given property type",
+		"Cannot specify DefaultValue for property type {0}",
 		Category,
 		DiagnosticSeverity.Error,
 		true,
-		"DefaultValue not supported for the given property type.");
+		"Cannot specify DefaultValue on for properties with types not supported by attributes.");
 
 	public static readonly DiagnosticDescriptor BindablePropertyDefaultValueAndFactory = new(
-		"DNU0006",
-		"Can't specify a value for DefaultValue while DefaultValueFactory is true for [BindableProperty]",
-		"Can't specify a value for DefaultValue while DefaultValueFactory is true",
+		Prefix + "0006",
+		"Value of DefaultValue will not be used",
+		"Setting DefaultValue will have no effect when DefaultValueFactory is true",
 		Category,
-		DiagnosticSeverity.Error,
+		DiagnosticSeverity.Warning,
 		true,
-		"Can't specify a value for DefaultValue while DefaultValueFactory is true.");
+		"The value passed into DefaultValue will be ignored if DefaultValueFactory is true.");
 
 	public static void Add(this ImmutableArray<Diagnostic>.Builder builder, DiagnosticDescriptor descriptor, SyntaxReference? owner, params object?[] messageArgs)
 	{

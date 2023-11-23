3c1f5b42541276c0fb974dbbbc246e1c2d146194
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-11-23 20:34:52 +0000

    Removd old generators and old style attributes, add new BindablePropertyAttribute class

--- Pick.Net.Utilities.Maui.SourceGenerators/Generators/BindablePropertyGeneratorEntry.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Immutable;
-
-using Pick.Net.Utilities.Maui.SourceGenerators.Syntax;
-
-namespace Pick.Net.Utilities.Maui.SourceGenerators.Generators;
-
-internal sealed record BindablePropertyGeneratorEntry(ClassInfo ClassInfo, ImmutableArray<BindablePropertySyntaxGenerator> Properties, ImmutableArray<Diagnostic> Diagnostics);
--- Pick.Net.Utilities.Maui.TestApp/Controls/DefaultValueConversions.cs
+++ Pick.Net.Utilities.Maui.TestApp/Controls/DefaultValueConversions.cs
@@ -2,13 +2,54 @@
 
 namespace Pick.Net.Utilities.Maui.TestApp.Controls;
 
-[BindableProperty<byte>("Byte", DefaultValue = (byte)5)]
-[BindableProperty<double>("DecimalInt", DefaultValue = 10)]
-[BindableProperty<double>("DecimalLong", DefaultValue = 10L)]
-[BindableProperty<double>("DecimalDouble", DefaultValue = 5.5d)]
-[BindableProperty<float>("FloatInt", DefaultValue = 10)]
-[BindableProperty<float>("FloatLong", DefaultValue = 10L)]
-[BindableProperty<uint>("IntUint", DefaultValue = 5u)]
 internal partial class DefaultValueConversions : BindableObject
 {
+	[BindableProperty(DefaultValue = (byte)5)]
+	public byte Byte
+	{
+		get => (byte)GetValue(ByteProperty);
+		set => SetValue(ByteProperty, value);
+	}
+
+	[BindableProperty(DefaultValue = 10)]
+	public double DecimalInt
+	{
+		get => (double)GetValue(DecimalIntProperty);
+		set => SetValue(DecimalIntProperty, value);
+	}
+
+	[BindableProperty(DefaultValue = 10L)]
+	public double DecimalLong
+	{
+		get => (double)GetValue(DecimalLongProperty);
+		set => SetValue(DecimalLongProperty, value);
+	}
+
+	[BindableProperty(DefaultValue = 5.5d)]
+	public double DecimalDouble
+	{
+		get => (double)GetValue(DecimalDoubleProperty);
+		set => SetValue(DecimalDoubleProperty, value);
+	}
+
+	[BindableProperty(DefaultValue = 10)]
+	public float FloatInt
+	{
+		get => (float)GetValue(FloatIntProperty);
+		set => SetValue(FloatIntProperty, value);
+	}
+
+	[BindableProperty(DefaultValue = 10L)]
+	public float FloatLong
+	{
+		get => (float)GetValue(FloatLongProperty);
+		set => SetValue(FloatLongProperty, value);
+	}
+
+	[BindableProperty(DefaultValue = 5u)]
+	public uint IntUint
+	{
+		get => (uint)GetValue(IntUintProperty);
+		set => SetValue(IntUintProperty, value);
+	}
 }

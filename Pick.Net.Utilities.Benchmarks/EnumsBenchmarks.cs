aa008d7e0cce7a4a6741175ca10ca075f714e32e
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-12-08 19:37:32 +0000

    Added benchmarks project

---
+++ Pick.Net.Utilities.Benchmarks/EnumsBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Pick.Net.Utilities.Benchmarks;
+
+[MemoryDiagnoser]
+public class EnumsBenchmarks
+{
+	public const BindingFlags Flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.GetProperty | BindingFlags.InvokeMethod;
+
+	[Benchmark(Baseline = true)]
+	public void HasFlags()
+	{
+		foreach (var value in Enums.GetValues<BindingFlags>())
+			Flags.HasFlag(value);
+	}
+
+	[Benchmark]
+	public void HasFlagsFast()
+	{
+		foreach (var value in Enums.GetValues<BindingFlags>())
+			Flags.HasFlagsFast(value);
+	}
+}
---
+++ Pick.Net.Utilities.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+using Pick.Net.Utilities.Benchmarks;
+
+var config = DefaultConfig.Instance
+	.AddDiagnoser(MemoryDiagnoser.Default);
+//	.AddJob(Job.Default.WithRuntime(CoreRuntime.Core70));
+
+BenchmarkRunner.Run<EnumsBenchmarks>(config);

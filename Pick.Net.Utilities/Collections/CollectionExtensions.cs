364ff9a0edf7721a7fbfbb52a4c266da364d1ffa
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-12-09 20:15:24 +0000

    Removed MapExtensions

--- Pick.Net.Utilities/Collections/CollectionExtensions.cs
+++ Pick.Net.Utilities/Collections/CollectionExtensions.cs
@@ -5,10 +5,18 @@ public static class CollectionExtensions
 	public static void Deconstruct<T, TKey, TValue>(this T pair, out TKey key, out TValue value)
 		where TKey : notnull
 		where TValue : class
-		where T : IMapEntry<TKey, TValue>
+		where T : IReadOnlyMapEntry<TKey, TValue>
 	{
 		key = pair.Key;
 		value = pair.Value;
+	}
+
+	public static void Deconstruct<TKey, TValue>(this IReadOnlyMapEntry<TKey, TValue> entry, out TKey key, out TValue value)
+		where TKey : notnull
+		where TValue : class
+	{
+		key = entry.Key;
+		value = entry.Value;
 	}
 
 	public static void Insert<T1, T2>(this IList<ValueTuple<T1, T2>> @this, int index, T1 item1, T2 item2)
@@ -49,5 +57,4 @@ public static class CollectionExtensions
 
 	public static void Add<T1, T2, T3, T4, T5, T6, T7>(this ICollection<ValueTuple<T1, T2, T3, T4, T5, T6, T7>> @this, T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7)
 		=> @this.Add((item1, item2, item3, item4, item5, item6, item7));
-
 }

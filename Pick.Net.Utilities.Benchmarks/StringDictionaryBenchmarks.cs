97e10068ec983ed0b81a5021c70dab9995537518
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-12-11 20:54:38 +0000

    Added StringDictionary benchmark

---
+++ Pick.Net.Utilities.Benchmarks/StringDictionaryBenchmarks.cs
@@ -0,0 +1,46 @@
+﻿namespace Pick.Net.Utilities.Benchmarks;
+
+using BenchmarkDotNet.Attributes;
+
+using Pick.Net.Utilities.Collections;
+
+using Kvp = KeyValuePair<string, string>;
+
+[MemoryDiagnoser]
+public class StringDictionaryBenchmarks
+{
+	private const int Count = 100;
+	public static readonly Kvp[] Pairs = Enumerable.Range(1, Count).Select(v => new Kvp("key" + v, "value" + v)).ToArray();
+	public static readonly string JoinedKeys = string.Join(';', Pairs.Select(v => v.Key));
+
+	[Benchmark(Baseline = true)]
+	public void NormalDictionary()
+	{
+		var dict = new Dictionary<string, string>(Pairs);
+
+		for (var i = 0; i < Count;)
+		{
+			var key = "key" + (++i);
+			var value = dict.ContainsKey(key);
+		}
+	}
+
+	[Benchmark]
+	public void StringDictionary()
+	{
+		var dict = new StringDictionary<string>(Pairs);
+		var startIndex = 0;
+		var charCount = 4;
+
+		for (var i = 0; i < Count;)
+		{
+			if (++i == 10 || i == 100)
+				charCount++;
+
+			var key = JoinedKeys.AsSpan(startIndex, charCount);
+			var value = dict.ContainsKey(key);
+
+			startIndex += charCount + 1;
+		}
+	}
+}

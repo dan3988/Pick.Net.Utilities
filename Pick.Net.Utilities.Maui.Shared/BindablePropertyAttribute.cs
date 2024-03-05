148b7dd9a4885d3c926285cc0fab74f86547a88a
dan3988 <40858622+dan3988@users.noreply.github.com>
2024-03-05 10:58:44 +0000

    Added XML docs for public classes and members

--- Pick.Net.Utilities.Maui.Shared/BindablePropertyAttribute.cs
+++ Pick.Net.Utilities.Maui.Shared/BindablePropertyAttribute.cs
@@ -1,14 +1,44 @@
 ﻿namespace Pick.Net.Utilities.Maui;
 
+/// <summary>
+/// Use to indicate that the annotated property/method is an accessor for an instance/attached bindable property and a backing <see cref="BindableProperty"/> should be generated.
+/// <para>
+/// The attribute can be used to generate instance properties
+/// <code>
+/// partial class MyBindableObject : BindableObject
+/// {
+///		[BindableProperty]
+///		public string MyValue
+///		{
+///			get => GetValue(MyValueProperty);
+///			set => SetValue(MyValueProperty, value);
+///		}
+/// }
+/// </code>
+/// This will generate a partial class with the static read-only field <c>MyValueProperty</c>.
+/// </para>
+/// </summary>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
 public sealed class BindablePropertyAttribute : Attribute
 {
+	/// <summary>
+	/// The name of a property/field that should be passed as the value for <see cref="BindableProperty.DefaultValue"/>, or a method that can be used as the <see cref="BindableProperty.CreateDefaultValueDelegate"/> for the property.
+	/// </summary>
 	public string? DefaultValue { get; init; }
 
+	/// <summary>
+	/// The value of <see cref="BindableProperty.DefaultBindingMode"/> for the property.
+	/// </summary>
 	public BindingMode DefaultMode { get; init; } = BindingMode.OneWay;
 
+	/// <summary>
+	/// Whether to generate a method to pass in as the <see cref="BindableProperty.CoerceValueDelegate"/> of the property.
+	/// </summary>
 	public bool CoerceValueCallback { get; init; }
 
+	/// <summary>
+	/// Whether to generate a method to pass in as the <see cref="BindableProperty.ValidateValueDelegate"/> of the property.
+	/// </summary>
 	public bool ValidateValueCallback { get; init; }
 
 	public BindablePropertyAttribute()
--- Pick.Net.Utilities.Maui/MarkupExtensions/TrueExtension.cs
+++ Pick.Net.Utilities.Maui/MarkupExtensions/TrueExtension.cs
@@ -1,5 +1,11 @@
 ﻿namespace Pick.Net.Utilities.Maui.MarkupExtensions;
 
+/// <summary>
+/// Returns a boxed <c>true</c> boolean value.
+/// </summary>
+/// <remarks>
+/// Useful use in cases when a boolean value is needed but the property type is <see cref="object"/>, e.g. <see cref="BindingExtension.FallbackValue"/>.
+/// </remarks>
 [AcceptEmptyServiceProvider]
 public sealed class TrueExtension : IMarkupExtension<bool>
 {

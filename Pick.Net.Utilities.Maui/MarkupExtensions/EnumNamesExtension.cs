5c53b03d1ae8f4be81c56f033a5bf94604d0d313
dan3988 <40858622+dan3988@users.noreply.github.com>
2024-03-07 16:02:57 +0000

    Added EnumValues and EnumNames markup extensions

---
+++ Pick.Net.Utilities.Maui/MarkupExtensions/EnumNamesExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace Pick.Net.Utilities.Maui.MarkupExtensions;
+
+/// <summary>
+/// Returns a list of the names of the enum constants declared in the specified enum type.
+/// </summary>
+[ContentProperty(nameof(Type))]
+[AcceptEmptyServiceProvider]
+public sealed class EnumNamesExtension : IMarkupExtension<IList<string>>
+{
+	/// <summary>
+	/// The type of enum to get values for
+	/// </summary>
+	public required Type Type { get; init; }
+
+	/// <inheritdoc/>
+	public IList<string> ProvideValue(IServiceProvider serviceProvider)
+		=> Enums.GetNameList(Type);
+
+	object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+		=> ProvideValue(serviceProvider);
+}
---
+++ Pick.Net.Utilities.Maui/MarkupExtensions/EnumValuesExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Pick.Net.Utilities.Maui.MarkupExtensions;
+
+/// <summary>
+/// Returns a list of enum constants declared in the specified enum type.
+/// </summary>
+[ContentProperty(nameof(Type))]
+[AcceptEmptyServiceProvider]
+public sealed class EnumValuesExtension : IMarkupExtension<IList>
+{
+	/// <summary>
+	/// The type of enum to get values for
+	/// </summary>
+	public required Type Type { get; init; }
+
+	/// <inheritdoc/>
+	public IList ProvideValue(IServiceProvider serviceProvider)
+		=> Enums.GetValueList(Type);
+
+	object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+		=> ProvideValue(serviceProvider);
+}
